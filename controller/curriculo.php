739b9867c260cfedb1f1089b5d359b7e1a3da161
MaryaVic <mferreiradeoliveira749@gmail.com>
2025-07-29 09:26:15 -0300

    Update curriculo.php

--- controller/curriculo.php
+++ controller/curriculo.php
@@ -1,1 +1,51 @@
+<?php
+$nome = $_POST["nome"];
+$telefone = $_POST["telefone"];
+$email = $_POST["email"];
+$endereco = $_POST["endereco"];
 
+$efc = $_POST["efc"];
+$emc = $_POST["emc"];
+$esc = $_POST["esc"];
+$pgc = $_POST["pgc"];
+$mc = $_POST["mc"];
+
+$empresa = $_POST["empresa"];
+$data_admissao = $_POST["data_admissao"];
+$data_demissao = $_POST["data_demissao"];
+$atividade_exercida = $_POST["atividade_exercida"];
+
+$descricao = $_POST["descricao"];
+$horas = $_POST["horas"];
+$ano_conclusao = $_POST["ano_conclusao"];
+
+$informacoes = $_POST["informacoes"];
+
+echo "DADOS PESSOAIS" 
+echo $nome;
+echo $telefone;
+echo $email;
+echo $endereco;
+
+
+echo "ESCOLARIDADE";
+echo $efc;
+echo $emc;
+echo $esc;
+echo $pgc;
+echo $mc;
+
+echo "EXPERIÊNCIA PROFISSIONAL";
+echo $empresa;
+echo $data_admissao;
+echo $data_demissao;
+echo $atividade_exercida;
+
+echo "CURSO E IDIOMAS";
+echo $descricao;
+echo $horas;
+echo $ano_conclusao;
+
+echo "INFORMAÇÕES ADICIONAIS";
+echo $informacoes;
+?>
